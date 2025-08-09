e4e1bbbf5a0bcfa208944fab72f50bfb03f2075c
Marcus Luebke <18036617+j1ng3r@users.noreply.github.com>
2025-08-09 14:27:40 -0700

    ignoring all old files

--- old/required.ts
+++
@@ -1,3 +0,0 @@
-export const rloaded = new Promise((res, rej) => window.addEventListener("load", res));
-export const rcss = document.fonts.load("25px Share Tech Mono");
-export const rlevels = fetch("src/levels.json").then(data => data.json());
